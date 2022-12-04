11871c2044d88f9a93aa6db65bededa072f9f1b1
mosafari <m.safari2680@gmail.com>
2022-12-04 16:26:14 +0330

    creating modelSerializer

--- Django-models/internetsession/srializers.py
+++ Django-models/internetsession/srializers.py
@@ -1,1 +1,10 @@
 # To convert the Model object to an API-appropriate format
+
+from rest_framework import serializers
+from .models import internet_seesion as IS
+
+class modelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IS
+        # fields of the model
+        fields = ["name" , "start_time" , "usage_time" , "ip" , "MAC" , "upload" , "download" , "total_transfers", "session_break_reason"]
